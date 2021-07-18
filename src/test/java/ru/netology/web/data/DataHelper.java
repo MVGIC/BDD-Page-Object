651298666b65fb8f71bfb0f641f169c542a82828
MVGIC <ex3ct@mail.ru>
2021-07-18 13:25:44 +0300

    tests added

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -42,17 +42,4 @@ public class DataHelper {
     public static CardInfo getSecondCardInfo() {
         return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
-
-   /* @Value
-    public static class CardId {
-        private String id;
-    }
-
-    public static CardId getFirstCardId() {
-        return new CardId("92df3f1c-a033-48e6-8390-206f6b1f56c0");
-    }
-
-    public static CardId getSecondCardId() {
-        return new CardId("0f3f5c2a-249e-4c3d-8287-09f7a039391d");
-    }*/
 }
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,10 +1,12 @@
 package ru.netology.web.test;
 
+import com.codeborne.selenide.Condition;
 import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.page.LoginPage;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.netology.web.data.DataHelper.*;
@@ -22,14 +24,56 @@ class MoneyTransferTest {
         val dashboardPageBeforeTransfer = new LoginPage()
                 .validLogin(getAuthInfo())
                 .validVerify(getVerificationCodeFor(getAuthInfo()));
-        int actualAmountFirstCard = dashboardPageBeforeTransfer.getCardBalance(getFirstCardInfo());
-        int actualAmountSecond = dashboardPageBeforeTransfer.getCardBalance(getSecondCardInfo());
+        int balanceBeforeTransferFirstCard = dashboardPageBeforeTransfer.getCardBalance(getFirstCardInfo());
+        int balanceBeforeTransferSecondCard = dashboardPageBeforeTransfer.getCardBalance(getSecondCardInfo());
         val dashboardPageAfterTransfer = dashboardPageBeforeTransfer
                 .addDepositTo(getSecondCardInfo())
                 .transferMoney(sum, getFirstCardInfo());
-        val balanceFirst = dashboardPageAfterTransfer.getCardBalance(getFirstCardInfo());
-        val balanceSecond = dashboardPageAfterTransfer.getCardBalance(getSecondCardInfo());
-        assertEquals(actualAmountFirstCard - sum, balanceFirst);
-        assertEquals(actualAmountSecond + sum, balanceSecond);
+        val balanceAfterTransferFirstCard = dashboardPageAfterTransfer.getCardBalance(getFirstCardInfo());
+        val balanceAfterTransferSecondCard = dashboardPageAfterTransfer.getCardBalance(getSecondCardInfo());
+        assertEquals(balanceBeforeTransferFirstCard - sum, balanceAfterTransferFirstCard);
+        assertEquals(balanceBeforeTransferSecondCard + sum, balanceAfterTransferSecondCard);
+    }
+
+    @Test
+    void shouldTransferMoneyFromSecondToFirst() {
+        val sum = 100;
+        val dashboardPageBeforeTransfer = new LoginPage()
+                .validLogin(getAuthInfo())
+                .validVerify(getVerificationCodeFor(getAuthInfo()));
+        int balanceBeforeTransferFirstCard = dashboardPageBeforeTransfer.getCardBalance(getFirstCardInfo());
+        int balanceBeforeTransferSecondCard = dashboardPageBeforeTransfer.getCardBalance(getSecondCardInfo());
+        val dashboardPageAfterTransfer = dashboardPageBeforeTransfer
+                .addDepositTo(getFirstCardInfo())
+                .transferMoney(sum, getSecondCardInfo());
+        val balanceAfterTransferFirstCard = dashboardPageAfterTransfer.getCardBalance(getFirstCardInfo());
+        val balanceAfterTransferSecondCard = dashboardPageAfterTransfer.getCardBalance(getSecondCardInfo());
+        assertEquals(balanceBeforeTransferFirstCard + sum, balanceAfterTransferFirstCard);
+        assertEquals(balanceBeforeTransferSecondCard - sum, balanceAfterTransferSecondCard);
+    }
+
+    @Test
+    void shouldLoginWithInvalidLogin() {
+        val loginPage = new LoginPage();
+        loginPage.invalidLogin(getOtherAuthInfo(getAuthInfo()));
+        $("[data-test-id=error-notification] .notification__content").shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    void shouldTransferExceedingAmountToFirst() {
+        val sum = 11000;
+        val dashboardPageBeforeTransfer = new LoginPage()
+                .validLogin(getAuthInfo())
+                .validVerify(getVerificationCodeFor(getAuthInfo()));
+        int balanceBeforeTransferFirstCard = dashboardPageBeforeTransfer.getCardBalance(getFirstCardInfo());
+        int balanceBeforeTransferSecondCard = dashboardPageBeforeTransfer.getCardBalance(getSecondCardInfo());
+        val dashboardPageAfterTransfer = dashboardPageBeforeTransfer
+                .addDepositTo(getFirstCardInfo())
+                .transferMoney(sum, getSecondCardInfo());
+        val balanceAfterTransferFirstCard = dashboardPageAfterTransfer.getCardBalance(getFirstCardInfo());
+        val balanceAfterTransferSecondCard = dashboardPageAfterTransfer.getCardBalance(getSecondCardInfo());
+        assertEquals(balanceBeforeTransferFirstCard + sum, balanceAfterTransferFirstCard);
+        assertEquals(balanceBeforeTransferSecondCard - sum, balanceAfterTransferSecondCard);
     }
 }
